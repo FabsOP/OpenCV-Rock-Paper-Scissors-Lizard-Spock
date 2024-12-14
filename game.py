63819907b7ec52f590edc7068fff9c841b0483a0
Fabio <fabiooryanpaulo@gmail.com>
2024-12-14 10:50:22 +0200

    add left and right hand detection

--- game.py
+++ game.py
@@ -1,38 +1,44 @@
 import cv2
 import cvzone
-from cvzone import HandTrackingModule
+from cvzone.HandTrackingModule import HandDetector
 
 ################## SET UP WEBCAM ######################
-cam = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
-cam.set(3,960)                        #camera width
-cam.set(4,540)                        #camera height
+camera.set(3,960)                        #camera width
+camera.set(4,540)                        #camera height
 
+############ CREATE HAND DETECTOR #######################
+detector = HandDetector(maxHands=1)
+
+############ FRAME UPDATE LOOP ##########################
 while True:
-    ############## Camera ##################################
-    #get camera img
-    success, img = cam.read()       #return success boolean and image
+    ##### get camera img ##########
+    success, cam = camera.read()       #return success boolean and image
     
-    #resize camera img 205/540
-    img = cv2.resize(img, (0,0), None, 0.379, 0.379)
+    ########## resize camera img 205/540 ##########
+    cam = cv2.resize(cam, (0,0), None, 0.379, 0.379)
     
-    #Flip camera accross the y-axis to mirror user correctly
-    img = cv2.flip(img, 1)
+    ##### Flip camera accross the y-axis to mirror user correctly ####
+    cam = cv2.flip(cam, 1)
     
-    #Crop camera img to fix in player box
-    img = img[:,80:283]
+    ##### Crop camera img to fix in player box ########
+    cam = cam[:,80:283]
         
-    #get background img and resize
+    ###### get background img and resize ######
     imgBG = cv2.imread("UI/bg.png")
     imgBG = cv2.resize(imgBG, (960, 540)) 
     
+    ########## Find hands and update image ######
+    hands, cam = detector.findHands(cam, draw=True, flipType=False)
+    
     
     ########## Place Camera img on bg img ###############
-    imgBG[199:404,598:801] = img
+    imgBG[199:404,598:801] = cam
     
     ########## Show images ####################
     #Show camera img
-    cv2.imshow("You", img)
+    # cv2.imshow("You", cam)
     
     #show background img
     cv2.imshow("bg", imgBG)
