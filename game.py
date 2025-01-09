004d853c3e717485ccd8305aea0d2e5159cec121
Fabio <fabiooryanpaulo@gmail.com>
2025-01-09 20:39:29 +0200

    add logic to toggle on and off ai model training mode. add logic to cycle between training slots (0-9) for training up to 10 hand signs

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import time
+# from model import KeyPointClassifier
+
+############# UI VARIABLES ###########################
+playerScore = 0
+aiScore = 0
+
+playerSign = ""
+aiSign = ""
+
+isTraining = False
+trainingSlot = '0'
 
 ################## SET UP WEBCAM ######################
 camera = cv2.VideoCapture(0)
@@ -11,8 +23,15 @@ camera.set(4,540)                        #camera height
 ############ CREATE HAND DETECTOR #######################
 detector = HandDetector(maxHands=1)
 
+############ LOAD MODEL ################################
+labels = ["rock", "paper", "scissors", "lizard", "spock"]
+# keypoint_classifier = KeyPointClassifier()
+
+
+
 ############ FRAME UPDATE LOOP ##########################
-while True:
+while True:  
+    
     ##### get camera img ##########
     success, cam = camera.read()       #return success boolean and image
     
@@ -31,6 +50,17 @@ while True:
     
     ########## Find hands and update image ######
     hands, cam = detector.findHands(cam, draw=True, flipType=False)
+
+    if hands:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        bbox = hand['bbox']
+        x_min, y_min, box_width, box_height = bbox
+        print('x_Min =', x_min)
+        print('y_Min =', y_min)
+        print('box_width =', box_width)
+        print('box_height =', box_height)
+        # print(hand['lmList'])
     
     
     ########## Place Camera img on bg img ###############
@@ -43,7 +73,26 @@ while True:
     #show background img
     cv2.imshow("bg", imgBG)
     
-    ################# End of Loop ###########
-    #refresh frequency
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    ################# Handle key presses ###########
+    key = cv2.waitKey(1) & 0xFF
+    
+    #Toggle Training Mode 
+    if key == ord('t'):  # Checks if 'T' is pressed
+        print("Key 't' pressed")
+        isTraining = not isTraining
+        
+        if isTraining:
+            print("Training mode activated")
+            print("(Training on slot #"+ trainingSlot+")")
+        else:
+            print("Training mode deactivated")
+    
+    #change training slot
+    elif isTraining and key in list(map(ord,['0','1','2','3','4','5','6','7','8','9'])):
+        trainingSlot = chr(key)
+        print("Training slot #" + trainingSlot + " selected")
+    
+    #end program        
+    elif key == ord('q'):
           break
+    
